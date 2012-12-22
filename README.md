aded57c6336049af69a15df90b1945ad8d363e49
Álvaro Moraleda <alvaro.moraleda@gmail.com>
2012-12-22 18:43:45 +0100

    Translate

--- README.md
+++ README.md
@@ -3,12 +3,34 @@ finite-automaton-simulator
 
 This program simulates three kind Finite Automaton: deterministic, nondeterministic and nondeterministic with epsilon transitions.
 
-Written in Caml Light as a practice to Automata Theory and Formal Languages class.
+Written in Caml Light as a practice to Automata Theory and Formal Languages class in 2004.
 
 
-(** HOW TO RUN THE PROGRAM **)
-On the Caml Light interpreter:
+How it works:
+=============
 
-- Copy the full code.
-- Execute: 
-	mainProgram();;
+Create a graph with tuples of values: start state, finish state and transition string. 
+ - A, B, 0
+ - A, C, 1
+ - B, B, 0
+ - B, C, 1
+ - C, A, 1
+ - C, D, 0
+ - D, D, 0
+ - D, D, 1
+
+Initial state: A
+
+Final state: D
+ 
+Then the user has to write (char to char) an input sequence to test the automaton.
+ Example:
+ - 1 
+ - 0
+ - 0 
+ - 1
+ - 0
+ - 1
+ - 0
+ 
+ In this case, the input is accepted. (Otherwise doesn't show any message)
--- automaton.ml
+++ automaton.ml
@@ -1,17 +1,9 @@
-(*Esta funcion tiene como parámetros el estado al que vamos a pasar, la cadena de entrada por la
- que nos encontramos y el recorrido llevado hasta ahora. Lo que hace es comprobar si ya hemos
- pasado por dicho estado y si hemos pasado, modifica la cadena con la que habíamos pasado 
- anteriormente, si no habíamos pasado, añade el estado y la cadena actual a la lista de recorrido*)
+(* Add the next state to the list of states already visited *)
 
 let rec addPath = function (estIni,cad,[]) -> [(estIni,cad)]
 	| (estIni,cad,(est2,cad2)::listRecorr) -> 
 			if (estIni=est2) then (est2,cad)::listRecorr
 			else (est2,cad2)::addPath(estIni,cad,listRecorr);;
-
-
-(*Recibe el estado al que vamos a pasar, la cadena de entrada actual y una lista con el recorrido
- que hemos realizado hasta el momento. Si no hemos pasado por ese estado o hemos pasado con una
- cadena diferente devuelve true, si hemos pasado con la misma cadena devuelve false*)
 
 let rec transitToNext = function (estIni,cad,[]) -> true
 	| (estIni,cad,(est2,cad2)::listRecorr) -> 
@@ -19,49 +11,28 @@ let rec transitToNext = function (estIni,cad,[]) -> true
 			else transitToNext(estIni,cad,listRecorr);;
 
 
-(*Recibe una cadena y una lista de estados disponibles del automata desde el estado actual. Compara
- la cadena con el primer elemento de la tupla de cada elemento de la lista y si es igual, devuelve
- el segundo elemento, que es una lista con todos los estados accesibles desde el estado actual 
- con la cadena de entrada *)
-
 let rec getList = function (cad,[]) -> []
 	| (cad,(cad2,listEst)::listComport) ->	if (cad=cad2) then listEst
 						else getList(cad,listComport);;
 
-
-(*Recibe un symbol de entrada y un autómata. Da la lista de los posibles estados a los que puede 
- pasar con dicho símbolo desde el estado en el que nos encontramos*)
-
-let rec getPossibleStates = function (_,_,[]) -> []
+let rec getNextPossibleStates = function (_,_,[]) -> []
 	| (cad,estIni,(estDest,listEst)::listComport) -> 
 		if (estIni=estDest) then getList(cad,listEst)
-		else getPossibleStates(cad,estIni,listComport);;
+		else getNextPossibleStates(cad,estIni,listComport);;
 		
 		
-(*Recibe un estado y una lista de estados aceptadores y comprueba que el estado que le hemos 
- pasado es un aceptador o no*)
-
 let rec isFinalState = function (est,[]) -> false
 	| (est,est2::listEstAcep) -> 	if (est=est2) then true
 					else isFinalState(est,listEstAcep);;
 
 
-(*Recibe una lista y quita el primer elemento. Si la lista está vacía devuelve la lista vacía*)
-
 let removeFirstElement = function [] -> []
 	| (elem::listaElem) -> listaElem;;
-
-
-(*Recibe un lista y devuelve el primer elemento de la cadena de entrada. Si la lista está vacía 
- devuelve caracter en blanco*)
 
 let getFirstElement = function [] -> ""
 	| elem::listaElem -> elem;;
 
-
-(*Recibe una lista de los caminos a seguir mediante transiciones vacías y un autómata. Una vez que se haya acabado la cadena
- de entrada, miramos las posibles soluciones únicamente con las transiciones vacías*)
-
+(* Check if is possible to solve the automaton with epsilon transitions *)
 let rec runOverEpsilonTransitions = function ([],automata) -> ()
 	| (est::lisTVacias,(estIni,listEstAcep,listComport,recorrido,cadEntrada)) ->
 		begin
@@ -73,13 +44,12 @@ let rec runOverEpsilonTransitions = function ([],automata) -> ()
 			end
 			else ();	
 			runOverEpsilonTransitions(lisTVacias,(estIni,listEstAcep,listComport,recorrido,cadEntrada));
-			runOverEpsilonTransitions(getPossibleStates("tVacia",est,listComport),(est,listEstAcep,listComport,addPath(est,cadEntrada,recorrido),cadEntrada));
+			runOverEpsilonTransitions(getNextPossibleStates("tVacia",est,listComport),(est,listEstAcep,listComport,addPath(est,cadEntrada,recorrido),cadEntrada));
 		end;;
 		
 
-(*Recibe como entrada un autómata compuesto de estado inicial, lista de estados aceptadores, comportamiento, 
- recorrido hasta el momento y cadena de entrada. Si la cadena de entrada es vacia, mira si el estado en el que
- nos encontramos es aceptador. Si la cadena no es vacia transitToNext los caminos posibles*)
+(* Gets an initial state, list of final states, behavior, path until current state, and an input string. If the input string is empty, test if the current state is
+in the list of final states. If the string is not empty, continues to the possible paths. *)
 
 let rec solveAutomaton (estIni,listEstAcep,listComport,recorrido,cadEntrada) = 
 		let rec recorrCaminos = function ([],[],automata) -> ()
@@ -101,18 +71,15 @@ let rec solveAutomaton (estIni,listEstAcep,listComport,recorrido,cadEntrada) =
 		begin
 			if (isFinalState(estIni,listEstAcep)) then
 			begin  
-				print_string "The automaton accepts the input string.";
+				print_string "The automaton accepts the input string. ";
 				print_string estIni;
 				print_string ".\n"
 			end
 			else ();
-			runOverEpsilonTransitions(getPossibleStates("tVacia",estIni,listComport),(estIni,listEstAcep,listComport,recorrido,[]));
+			runOverEpsilonTransitions(getNextPossibleStates("tVacia",estIni,listComport),(estIni,listEstAcep,listComport,recorrido,[]));
 		end
-		else recorrCaminos(getPossibleStates("tVacia",estIni,listComport),getPossibleStates(getFirstElement(cadEntrada),estIni,listComport),(estIni,listEstAcep,listComport,recorrido,cadEntrada));;
+		else recorrCaminos(getNextPossibleStates("tVacia",estIni,listComport),getNextPossibleStates(getFirstElement(cadEntrada),estIni,listComport),(estIni,listEstAcep,listComport,recorrido,cadEntrada));;
 
-
-
-(*Buscamos un elemento en una lista, si no está lo metemos, si está no hacemos nada*)
 
 let rec existElement =  function (elem1,[]) -> [elem1]
 	| (elem1,elem2::listaElem) -> 
@@ -121,21 +88,16 @@ let rec existElement =  function (elem1,[]) -> [elem1]
 
 
 
-(* Aumentamos el comportamiento del aut—mata *)
-
 let rec searchEquivalent = function (estFin,cad1,[]) ->[(cad1,[estFin])] 
 	| (estFin,cad1,(cad2,listEst)::listTup) ->	
 		if (cad1=cad2) then (cad2,existElement(estFin,listEst))::listTup
 		else (cad2,listEst)::searchEquivalent(estFin,cad1,listTup);;
 
-(* Introducimos en el comportamiento los estados que no tienen flechas de salida *)
 let rec insertState = function (estIni,estFin,[])-> [(estFin,[])]
 	|(estIni,estFin,(est,listTup)::listAut) -> 	if (estIni=estFin) then (est,listTup)::listAut
 							else (est,listTup)::insertState(estIni,estFin,listAut);; 
 
 
-	
-(* Validate tuple *)
 let rec validateTuple = function (estIni,estFin,cad,[]) ->[(estIni,[(cad,[estFin])]);(estFin,[])]
 	| (estIni,estFin,cad,(est1,listTup)::listAut) -> 	
 		if (estIni=est1) then (est1,searchEquivalent(estFin,cad,listTup))::insertState(estIni,estFin,listAut)
